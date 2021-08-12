be5ec0b1cc95f0efd56bd6a90e4c647f1e8d4dd6
AlexdwiProject <86504958+AlexdwiProject@users.noreply.github.com>
2021-08-13 06:59:43 +0700

    Update kenalan.py

--- userbot/modules/kenalan.py
+++ userbot/modules/kenalan.py
@@ -6,11 +6,11 @@ from userbot.events import register
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("`Hai Perkenalkan Namaku Ramadhani`")
+    await typew.edit("`Hai Perkenalkan Namaku Alex`")
     sleep(3)
-    await typew.edit("20 Tahun`")
+    await typew.edit("`Umur 17 Tahun`")
     sleep(1)
-    await typew.edit("`Tinggal Di tangerang, Salam Kenal:)`")
+    await typew.edit("`Tinggal Di Surabaya, Salam Kenal Ka🥺..`")
 # Create by myself @localheart
 
 
